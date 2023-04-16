1fe27f9f723597a7232a346c4a943e649af64a7d
JAYACHANDRU K <84260854+jayachandru001@users.noreply.github.com>
2023-04-16 17:55:04 +0530

    Update built.sh

--- built.sh
+++ built.sh
@@ -3,11 +3,10 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
 virtualenv ivenv
 source ivenv/bin/activate
 pip install -r requirements.txt
+pip install --upgrade pip
 
 export FLASK_APP = img_compressor.py
 export FLASK_ENV = development
