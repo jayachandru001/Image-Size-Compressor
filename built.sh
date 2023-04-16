d687eecd7f59d6cdda0009ed5b7ffe24f5913843
JAYACHANDRU K <84260854+jayachandru001@users.noreply.github.com>
2023-04-16 17:55:48 +0530

    Update built.sh

--- built.sh
+++ built.sh
@@ -7,7 +7,3 @@ virtualenv ivenv
 source ivenv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
-
-export FLASK_APP = img_compressor.py
-export FLASK_ENV = development
-export flask run
